d92fdf02fb316f5407ffbf49c03fc9a6b79b9ba3
unknown <rkganeshan2010@gmail.com>
2021-04-01 08:40:56 +0530

    added front

---
+++ ecommerce-front/src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Signin=()=>(
+    <Layout title="Signin" description="Signin toNode React E Comm App">
+        ...
+    </Layout>
+)
+
+export default Signin
+
+
+
